4215625120eab16e34683307b20fc8dbc75c3756
Nozell <153051470+ImJustNozell@users.noreply.github.com>
2024-10-10 09:08:17 -0500

    Update WarJoinCommand.php

--- src/Nozell/ClanWar/commands/WarJoinCommand.php
+++ src/Nozell/ClanWar/commands/WarJoinCommand.php
@@ -10,6 +10,9 @@ use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat as TF;
 use Nozell\ClanWar\Main;
+use rxduz\factions\player\FactionPlayer;
+use rxduz\factions\player\PlayerManager;
+use rxduz\factions\faction\FactionManager;
 
 class WarJoinCommand extends BaseSubCommand
 {
@@ -21,6 +24,7 @@ class WarJoinCommand extends BaseSubCommand
     public function onRun(CommandSender $sender, string $label, array $args): void
     {
         $main = Main::getInstance();
+
         if (!$sender instanceof Player) {
             $sender->sendMessage(TF::RED . "Este comando solo puede ser usado por jugadores.");
             return;
@@ -31,20 +35,42 @@ class WarJoinCommand extends BaseSubCommand
             return;
         }
 
-
         $session = $main->getWarFactory()->getPlayerSession($sender);
+
         if ($session !== null && $session->isParticipant()) {
             $sender->sendMessage(TF::RED . "Ya estás participando en la guerra.");
             return;
         }
 
+        $factionPlayer = PlayerManager::getInstance()->getSessionByName($sender->getName());
+
+        if (!$factionPlayer instanceof FactionPlayer || !$factionPlayer->inFaction()) {
+            $sender->sendMessage(TF::RED . "No puedes unirte a la guerra si no perteneces a una facción.");
+            return;
+        }
+
+        $factionName = $factionPlayer->getFaction();
+        $faction = FactionManager::getInstance()->getFactionByName($factionName);
+
+        if ($faction === null) {
+            $sender->sendMessage(TF::RED . "Hubo un error al intentar obtener tu facción. Inténtalo de nuevo.");
+            return;
+        }
+
+        if (isset($main->getWarFactory()->getClans()[$factionName])) {
+
+            $main->getWarFactory()->addClan($factionName, $sender);
+            $sender->sendMessage(TF::GREEN . "Te has unido a la guerra como miembro de la facción $factionName.");
+        } else {
+
+            $main->getWarFactory()->addClan($factionName, $sender);
+            $sender->sendMessage(TF::GREEN . "Has creado y te has unido al clan de la facción $factionName.");
+        }
 
         $newSession = new PlayerSession($sender);
+        $newSession->setClanName($factionName);
         $main->getWarFactory()->addPlayerSession($sender, $newSession);
 
-
         $main->getWarFactory()->sendPlayerToArena($sender);
-
-        $sender->sendMessage(TF::GREEN . "Te has unido a la guerra de clanes.");
     }
 }
